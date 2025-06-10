29b8ab8a6ba76855eed89fb1c1be3e4fa47e3fb6
Srayan Jana <valorzard.kiko@outlook.com>
2025-06-10 05:18:27 -0700

    got subsecond working

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
-use egor::{app::App, Color, KeyCode};
+use dioxus_devtools::subsecond;
+use egor::{Color, KeyCode, app::App};
 
 fn main() {
     let mut player_x = 0.0;
     let mut player_y = 0.0;
+    // initialize subsecond for hot reloading
+    dioxus_devtools::connect_subsecond();
+
     App::init(|ctx| {
         ctx.set_title("Minimal Shooter");
     })
     .run(move |t, g, i| {
-        g.clear(Color::WHITE);
+        // Use subsecond to run the game loop with hot reloading
+        subsecond::call(|| {
+            g.clear(Color::WHITE);
 
-        let dx = i.keys_held(&[KeyCode::KeyD, KeyCode::ArrowRight]) as i8
-            - i.keys_held(&[KeyCode::KeyA, KeyCode::ArrowLeft]) as i8;
-        let dy = i.keys_held(&[KeyCode::KeyS, KeyCode::ArrowDown]) as i8
-            - i.keys_held(&[KeyCode::KeyW, KeyCode::ArrowUp]) as i8;
-        //let moving = dx != 0 || dy != 0;
+            let dx = i.keys_held(&[KeyCode::KeyD, KeyCode::ArrowRight]) as i8
+                - i.keys_held(&[KeyCode::KeyA, KeyCode::ArrowLeft]) as i8;
+            let dy = i.keys_held(&[KeyCode::KeyS, KeyCode::ArrowDown]) as i8
+                - i.keys_held(&[KeyCode::KeyW, KeyCode::ArrowUp]) as i8;
+            //let moving = dx != 0 || dy != 0;
 
-        player_x += dx as f32 * 200.0 * t.delta;
-        player_y += dy as f32 * 200.0 * t.delta;
-        g.camera().target(player_x, player_y);
+            player_x += dx as f32 * 200.0 * t.delta;
+            player_y += dy as f32 * 200.0 * t.delta;
+            g.camera().target(player_x, player_y);
 
+            g.rect()
+                .at(player_x, player_y)
+                .size(64., 64.)
+                .color(Color::BLUE);
 
-        g.rect()
-            .at(player_x, player_y)
-            .size(64., 64.)
-            .color( Color::BLUE);
-
-        g.rect()
-            .at(20.0, 75.0)
-            .size(64., 64.)
-            .color(Color::RED);
+            g.rect().at(50.0, 105.0).size(64., 64.).color(Color::RED);
+        });
     });
 }
